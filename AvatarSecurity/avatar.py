5240d96e1772e9c63405efe26942f3c995af9e84
Yury Barichev <bar2104y@yandex.ru>
2019-08-24 01:52:03 +0300

    Прокомментированно

--- AvatarSecurity/avatar.py
+++ AvatarSecurity/avatar.py
@@ -1,38 +1,49 @@
+#Загрузка необходимых модулей
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api import VkUpload
 
+# Перменная-костыль для избежания рекурсии
 IsMyUpdate = False
 
 def main():
-    vk_token=""
-    app_id = ""
-    vk_client_secret = ""
+    vk_token="" # Токен пользователя
+    app_id = "" # ID приложения
+    vk_client_secret = "" # Ключ приложения 
+    # Авторизация
     vk_session = vk_api.VkApi(token=vk_token, app_id=app_id, client_secret=vk_client_secret)
     vk = vk_session.get_api()
 
-    global IsMyUpdate
+    global IsMyUpdate # Объявления костыля как глобального
+    chat_id = "" # id отслеживаемого чата
+
+    # Подключение дополнительных библиотек
     longpoll = VkLongPoll(vk_session)
     upload = VkUpload(vk_session) 
 
+    # Перебор данных
     for event in longpoll.listen():
         if event.type == VkEventType.CHAT_UPDATE:
-            print(event.__dict__)
-            print(event.update_type)
-            print(event.update_type.__dict__)
+            # print(event.__dict__)
+            # print(event.update_type)
+            # print(event.update_type.__dict__)
             
+            # Если не мы меняли аватар
             if not IsMyUpdate:
                 try:
-                    r = upload.photo_chat(photo='gerb.png', chat_id='111')
-                    print(r)
-                    IsMyUpdate = True               
+                    # Загружаем новую
+                    r = upload.photo_chat(photo='gerb.png', chat_id=chat_id)
+                    #print(r)
+                    # Отмечаем, что это мы, теперь это смена автара не обработается из за условия вше
+                    IsMyUpdate = True           
                 except Exception as e:
-                    print(e)
+                    print(e) # выводим ошибки, если таковые имеются
             else:
-                IsMyUpdate = False
+                IsMyUpdate = False # Сбрасываем костыль
         # else:
         #     print(event.type)
 
+#Бесконечный цикл с обработкой ошибок
 while True:
     try:
         main()
